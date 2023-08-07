334bb3267c62b616346a85c64afea72abe38096a
poonam870 <94158523+poonam870@users.noreply.github.com>
2023-08-07 23:08:01 +0530

    Create camelcase.cpp

---
+++ camelcase.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+void convertCamelCase(string& s) {
+    string currentWord;
+    for (char c : s) {
+        if (islower(c)) {
+            currentWord+=toupper(c);
+        } else {
+            if (!currentWord.empty()) {
+                cout << currentWord <<endl;
+                currentWord.clear();
+            }
+            if (isupper(c)) {
+                currentWord +=tolower(c);
+            } else {
+                currentWord +=toupper(c);
+            }
+        }
+    }
+    if (!currentWord.empty()) {
+        cout << currentWord <<endl;
+    }
+}
+int main() {
+    string s = "aAnThe";
+    convertCamelCase(s);
+    return 0;
+}
